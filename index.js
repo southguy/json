6f8277c5c566475cef1e3609c318cced83bc5d73
Mano <101385626+southguy@users.noreply.github.com>
2023-06-04 07:57:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const jsonServer = require('json-server');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 
 // Serve static files from the "public" directory
 app.use(express.static('public'));
 
 // JSON Server setup
-const jsonServerRouter = jsonServer.router('public/club.json');
-const jsonServerMiddlewares = jsonServer.defaults();
+const jsonFilePath = path.join(__dirname, 'public', 'club.json');
 
-app.use(jsonServerMiddlewares);
-app.use(jsonServer.bodyParser);
-app.use('/', jsonServerRouter); // Set the base URL for JSON Server
+if (fs.existsSync(jsonFilePath)) {
+  const jsonServerRouter = jsonServer.router(jsonFilePath);
+  const jsonServerMiddlewares = jsonServer.defaults();
+
+  app.use(jsonServerMiddlewares);
+  app.use(jsonServer.bodyParser);
+  app.use('/api', jsonServerRouter); // Set the base URL for JSON Server
+} else {
+  console.error(`File not found: ${jsonFilePath}`);
+}
 
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
 module.exports = app;
